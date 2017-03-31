51de45eb2b846ff1809e9ddb5f9aa5d80980bc10
Exige <Exige@172.18.16.250>
2017-03-31 18:29:25 +0200

    Initilisation du projet.

---
+++ jaxb/src/main/java/fr/utbm/jaxb/App.java
@@ -0,0 +1,13 @@
+package fr.utbm.jaxb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
