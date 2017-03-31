5909dab0f83f9fb925466f745fc430f09ae31440
micka <micka@172.18.17.16>
2017-03-31 18:40:11 +0200

    test

--- jaxb/src/main/java/fr/utbm/jaxb/App.java
+++ jaxb/src/main/java/fr/utbm/jaxb/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Salut JE !" );
+        System.out.println( "Salut JE ! On va y arriver" );
     }
 }
