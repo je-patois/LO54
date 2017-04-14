6c836bddcb2c22e6aebac05104f4fe93de7b0cc2
Exige <Exige@172.18.16.250>
2017-04-14 17:51:30 +0200

    Essai de mise en place DB.

--- jaxb/src/main/java/fr/utbm/jaxb/App.java
+++ jaxb/src/main/java/fr/utbm/jaxb/App.java
@@ -1,13 +1,47 @@
 package fr.utbm.jaxb;
 
-/**
- * Hello world!
- *
- */
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import fr.utbm.jaxb.util.HibernateUtil;
+
 public class App 
 {
-    public static void main( String[] args )
+	public static void main( String[] args ) {
+		System.out.println("Bonjour");
+		
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		System.out.println("aaa");
+		/*try { 
+			System.out.println("3");
+			session.close(); 
+		} catch(Exception e) {
+			System.out.println("Fail : " + e);
+		}*/
+		
+    /* public static void main( String[] args )
     {
-        System.out.println( " On y arriver " );
-    }
+    	Session session = HibernateUtil.getSessionFactory().openSession();
+    try {
+        session.beginTransaction();
+        
+        session.getTransaction().commit();
+	}
+	catch (HibernateException he) {
+        he.printStackTrace();
+        if(session.getTransaction() != null) {
+            try {
+                session.getTransaction().rollback();
+            }catch(HibernateException he2) {he2.printStackTrace(); }
+        }
+	}
+	finally {
+        if(session != null) {
+            try { session.close(); } catch(Exception e) { System.out.println(e); }
+            }
+        }
+        
+    } */
+	System.out.println("Au revoir");	
+	}
 }
