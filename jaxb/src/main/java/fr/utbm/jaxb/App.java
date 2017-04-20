95303a09bc687f3ce20bcea50fbab4bc29d4e7b7
micka <micka@Mickal>
2017-04-20 16:16:36 +0200

    Mise en place d'un main plus professionnel
    - suppression du code mort
    - suppression des messages inutiles

--- jaxb/src/main/java/fr/utbm/jaxb/App.java
+++ jaxb/src/main/java/fr/utbm/jaxb/App.java
@@ -8,40 +8,6 @@ import fr.utbm.jaxb.util.HibernateUtil;
 public class App 
 {
 	public static void main( String[] args ) {
-		System.out.println("Bonjour");
-		System.out.println("Ca va ?");
 		Session session = HibernateUtil.getSessionFactory().openSession();
-		System.out.println("aaa");
-		/*try { 
-			System.out.println("3");
-			session.close(); 
-		} catch(Exception e) {
-			System.out.println("Fail : " + e);
-		}*/
-		
-    /* public static void main( String[] args )
-    {
-    	Session session = HibernateUtil.getSessionFactory().openSession();
-    try {
-        session.beginTransaction();
-        
-        session.getTransaction().commit();
-	}
-	catch (HibernateException he) {
-        he.printStackTrace();
-        if(session.getTransaction() != null) {
-            try {
-                session.getTransaction().rollback();
-            }catch(HibernateException he2) {he2.printStackTrace(); }
-        }
-	}
-	finally {
-        if(session != null) {
-            try { session.close(); } catch(Exception e) { System.out.println(e); }
-            }
-        }
-        
-    } */
-	System.out.println("Au revoir");	
 	}
 }
