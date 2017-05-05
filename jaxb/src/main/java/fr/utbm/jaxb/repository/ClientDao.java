f65cc0913d60ee528e7fd8b20ad3b0cbb1d3a589
mickaël <mickael.constanzer@laposte.net>
2017-05-05 14:53:51 +0200

    Ajout classes ClientDao et CourseDao

---
+++ jaxb/src/main/java/fr/utbm/jaxb/repository/ClientDao.java
@@ -0,0 +1,64 @@
+package fr.utbm.jaxb.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import fr.utbm.jaxb.util.HibernateUtil;
+import fr.utbm.jaxb.entity.*;
+
+public class ClientDao {
+
+	private Session session; 
+	
+	
+	// constructeur
+	public ClientDao() {
+		
+	}
+	
+	// Récupère toutes les informations de tous les clients 
+	public List<Client> getClient () {
+		
+		session = HibernateUtil.getSessionFactory().openSession();
+		List<Client> listeClient = new ArrayList<Client>();
+		
+		try {
+			session.beginTransaction();
+			Query query = session.createQuery("* from client");
+			listeClient = query.list();
+		}
+		
+		catch (HibernateException he) {
+	        he.printStackTrace();
+	        if(session.getTransaction() != null) {
+	            try {
+	                session.getTransaction().rollback();
+	            }catch(HibernateException he2) {
+	            	he2.printStackTrace(); 
+	            }
+	        }
+		}
+		
+		finally {
+		
+			if(session != null) {
+	            try { 
+	            	session.close();
+	            }catch(HibernateException he2) {
+	            	he2.printStackTrace(); 
+	            }
+	                
+			}
+	
+		}
+		
+		return listeClient;
+	}
+	
+	
+	
+}
---
+++ jaxb/src/main/java/fr/utbm/jaxb/repository/CourseDao.java
@@ -0,0 +1,62 @@
+package fr.utbm.jaxb.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import fr.utbm.jaxb.entity.Client;
+import fr.utbm.jaxb.entity.Course;
+import fr.utbm.jaxb.util.HibernateUtil;
+
+public class CourseDao {
+	
+	private Session session;
+	
+	public CourseDao() {
+		
+	}
+	
+	// Récupère toutes les informations de tous les clients 
+	public List<Course> getCourse (){
+			
+		session = HibernateUtil.getSessionFactory().openSession();
+		List<Course> listeCourse = new ArrayList<Course>();
+		
+		try {
+			session.beginTransaction();
+			Query query = session.createQuery("* from course");
+			listeCourse = query.list();
+		}
+		
+		catch (HibernateException he) {
+	        he.printStackTrace();
+	        if(session.getTransaction() != null) {
+	            try {
+	                session.getTransaction().rollback();
+	            }catch(HibernateException he2) {
+	            	he2.printStackTrace(); 
+	            }
+	        }
+		}
+		
+		finally {
+		
+			if(session != null) {
+	            try { 
+	            	session.close();
+	            }catch(HibernateException he2) {
+	            	he2.printStackTrace(); 
+	            }
+	                
+			}
+	
+		}
+		
+		return listeCourse;
+	}
+
+
+}
