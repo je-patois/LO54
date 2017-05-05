fa80287441c62a86877ae4e909b73dfad8340a7f
je-patois <jean-eudes.patois@utbm.fr>
2017-05-05 15:02:00 +0200

    2 autres DAO

---
+++ jaxb/src/main/java/fr/utbm/jaxb/repository/CourseSessionDAO.java
@@ -0,0 +1,60 @@
+package fr.utbm.jaxb.repository;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import fr.utbm.jaxb.entity.CourseSession;
+import fr.utbm.jaxb.entity.Location;
+import fr.utbm.jaxb.util.HibernateUtil;
+
+public class CourseSessionDAO implements Serializable {
+
+	private Session session = HibernateUtil.getSessionFactory().openSession();
+	
+	public CourseSessionDAO() {
+		
+	}
+
+	public Session getSession() {
+		return session;
+	}
+
+	public void setSession(Session session) {
+		this.session = session;
+	}
+	
+	public List<CourseSession> getCourseSession() {
+		List<CourseSession> courseSessionList = new ArrayList();
+	    try {
+	        getSession().beginTransaction();
+            Query query = getSession().createQuery("from CourseSession");
+            courseSessionList = query.list();
+	        getSession().getTransaction().commit();
+		}
+		catch (HibernateException he) {
+	        he.printStackTrace();
+	        if(getSession().getTransaction() != null) {
+	            try {
+	            	getSession().getTransaction().rollback();
+	            } catch(HibernateException he2) {
+	            	he2.printStackTrace();
+	            }
+	        }
+		}
+		finally {
+	        if(getSession() != null) {
+	            try {
+	            	getSession().close();
+	            } catch(Exception e) {
+	            	System.out.println(e);
+	            }
+            }
+       }
+       return courseSessionList;
+	}
+}
---
+++ jaxb/src/main/java/fr/utbm/jaxb/repository/LocationDAO.java
@@ -0,0 +1,59 @@
+package fr.utbm.jaxb.repository;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import fr.utbm.jaxb.entity.Location;
+import fr.utbm.jaxb.util.HibernateUtil;
+
+public class LocationDAO implements Serializable {
+
+	private Session session = HibernateUtil.getSessionFactory().openSession();
+	
+	public LocationDAO() {
+		
+	}
+	
+	public Session getSession() {
+		return session;
+	}
+
+	public void setSession(Session session) {
+		this.session = session;
+	}
+
+	public List<Location> getLocation() {
+		List<Location> locationList = new ArrayList();
+	    try {
+	        getSession().beginTransaction();
+            Query query = getSession().createQuery("from Location");
+            locationList = query.list();
+	        getSession().getTransaction().commit();
+		}
+		catch (HibernateException he) {
+	        he.printStackTrace();
+	        if(getSession().getTransaction() != null) {
+	            try {
+	            	getSession().getTransaction().rollback();
+	            } catch(HibernateException he2) {
+	            	he2.printStackTrace();
+	            }
+	        }
+		}
+		finally {
+	        if(getSession() != null) {
+	            try {
+	            	getSession().close();
+	            } catch(Exception e) {
+	            	System.out.println(e);
+	            }
+            }
+       }
+       return locationList;
+	}
+}
